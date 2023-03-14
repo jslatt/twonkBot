bf226e37b36bdc42f62a9cdc3c6c2240b57ad418
_lucidEntropy <james@lucidentropy.ooo>
2023-03-13 19:23:39 -0500

    Rarity

--- index.js
+++ index.js
@@ -205,6 +205,23 @@ client.on("ready", () => {
             }
             // Floor Bumps
             if (Math.abs(pctChg) >= 10.00 && floor > oldPrice) {
+
+              if (rarity == "Rare") {
+                client.channels.cache.get(process.env.CHANNEL).send(":arrow_up:** <@1066856218037075998> " + collections[objKey].name + "** " + floor.toFixed(2) + " (prev. " + oldPriceShort + ") [" + pctChg.toFixed(2) + "%] <@&1069999087585333298>" + pctFlag)
+              }
+
+              if (rarity == "Epic") {
+                client.channels.cache.get(process.env.CHANNEL).send(":arrow_up:** <@1066856171513843793> " + collections[objKey].name + "** " + floor.toFixed(2) + " (prev. " + oldPriceShort + ") [" + pctChg.toFixed(2) + "%] <@&1069999087585333298>" + pctFlag)
+              }
+
+              if (rarity == "Legendary") {
+                client.channels.cache.get(process.env.CHANNEL).send(":arrow_up:** <@1066856113796030484> " + collections[objKey].name + "** " + floor.toFixed(2) + " (prev. " + oldPriceShort + ") [" + pctChg.toFixed(2) + "%] <@&1069999087585333298>" + pctFlag)
+              }
+
+              if (rarity == "Exotic") {
+                client.channels.cache.get(process.env.CHANNEL).send(":arrow_up:** <@1066856009454329876> " + collections[objKey].name + "** " + floor.toFixed(2) + " (prev. " + oldPriceShort + ") [" + pctChg.toFixed(2) + "%] <@&1069999087585333298>" + pctFlag)
+              }
+              
   
               client.channels.cache.get(process.env.CHANNEL).send(":arrow_up:** " + rarity + " " + collections[objKey].name + "** " + floor.toFixed(2) + " (prev. " + oldPriceShort + ") [" + pctChg.toFixed(2) + "%] <@&1069999087585333298>" + pctFlag)
   
